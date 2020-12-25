6a0b2ab1ca531c11125069c45e3a8b903b27eaab
Hisho <>
2020-12-25 22:38:56 +0900

    🏷️ add types fro api/hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+type Response = {
+  statusCode: number
+  json(name: { name: string }): void
+}
+
+export default (_req: void, res: Response): void => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
