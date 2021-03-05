b0683927e2b72402cff6e91123aa21d82857d668
Hisho <hisho.web@gmail.com>
2021-03-05 19:25:00 +0900

    📦 add what-input

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@src/styles/globals.css';
 import React from 'react';
 import { AppProps } from 'next/app';
+import 'what-input';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
