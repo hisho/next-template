c8069b8d51a0cb90aea82d152a9fc7be25e61ad2
Hisho <hisho.web@gmail.com>
2021-03-06 23:49:05 +0900

    🔧 update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const path = require('path');
 
 module.exports = {
+  trailingSlash: true, //https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
   webpack(config) {
     config.resolve.alias['@src'] = path.join(__dirname, 'src');
     return config;
