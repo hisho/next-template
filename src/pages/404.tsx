1e50964e619a5e83307703f74dde8ad5d12f769a
Hisho <hisho.web@gmail.com>
2021-03-08 02:41:10 +0900

    ✨ update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { SEO, Layout } from '@src/layouts';
+import { PageContext, PageContextReducer } from '@src/store';
 
 const IndexPage: NextPage = () => {
+  const { pathname } = useRouter();
+  const [page, dispatch] = useReducer(PageContextReducer, {
+    path: pathname,
+  });
   return (
-    <>
-      <SEO
-        title={'404エラー　お探しのページは存在しません。'}
-        description={'404エラー お探しのページは存在しません。'}
-      />
+    <PageContext.Provider value={{ page, dispatch }}>
+      <SEO title={'タイトル'} description={'説明文'} />
       <Layout>
         <div>本文</div>
       </Layout>
-    </>
+    </PageContext.Provider>
   );
 };
 
