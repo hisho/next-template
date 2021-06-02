64c5fa5f8d3697c20d0e931a54e173fcb77180f0
hisho <hisho.web@gmail.com>
2021-06-02 19:10:55 +0900

    ✨ add configs

---
+++ src/configs/index.ts
@@ -0,0 +1,4 @@
+export * from './pages';
+export * from './siteMeta';
+export * from './variables';
+export * from './types';
---
+++ src/configs/variables.ts
@@ -0,0 +1,30 @@
+export const variables = {
+  breakpoints: {
+    xs: 600,
+    sm: 769,
+    md: 1000,
+    lg: 1200,
+    xl: 1366,
+  },
+  colors: {
+    primary: {
+      black: '#3B4043',
+      red: '#f00',
+      brown: '#5F5538',
+      gray: '#E3E1DA',
+      white: '#F0F3F5',
+    },
+    secondary: {
+      black: '#333',
+      gray: '#EFEDE9',
+    },
+  },
+  fontFamily: {
+    notosans: ['Noto Sans JP', 'sans-serif'],
+    notoserif: ['Noto Serif JP', 'serif'],
+  },
+} as const;
+
+export type variablesType = typeof variables;
+
+export type breakpointsNamesType = keyof variablesType['breakpoints'];
