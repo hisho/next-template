eea0194ad95bb266503051e30f269653fa23e080
hisho <hisho.web@gmail.com>
2021-06-02 19:11:17 +0900

    ✨ add hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export * from '@src/hooks/useSiteMeta';
+export * from '@src/hooks/usePage';
+export * from '@src/hooks/useSEO';
+export * from '@src/hooks/usePageReducer';
+export * from '@src/hooks/useVariables';
