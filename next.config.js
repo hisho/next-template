bb994d04eed60c40f13d050897cde3a02ce23e71
hisho <hisho.web@gmail.com>
2021-06-19 00:03:02 +0900

    📈 update comment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/* eslint-disable
-   @typescript-eslint/no-var-requires
-*/
 const path = require('path');
 
 module.exports = {
-  trailingSlash: true, //https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+  /**
+   * @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+   */
+  trailingSlash: true,
   webpack(config) {
     config.resolve.alias['@src'] = path.join(__dirname, 'src');
     return config;
